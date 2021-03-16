3adfc998602871e678df9cdfeba996f31750cef4
seang123 <seanpgies@gmail.com>
2021-03-16 13:05:03 +0100

    initial work on the cnn-rnn.py file; next step is to extract image features using pre-trained tensorflow vgg16

--- CNN_RNN/cnn_rnn.py
+++ CNN_RNN/cnn_rnn.py
@@ -1,3 +1,41 @@
 # Show and Tell | CNN RNN network implementation
 
+# import
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+import pickle
+from nsd_access import NSDAccess
+import sys
+sys.path.append('/home/seagie/NSD/Code/Masters-Thesis/')
+import utils
+import time
+import json
+import pandas as pd
+import tensorflow as tf
 
+
+# Select training set
+n_training_samples = 2 # /73k
+
+nsd_loader = NSDAccess("/home/seagie/NSD")
+
+nsd_loader.stim_descriptions = pd.read_csv(nsd_loader.stimuli_description_file, index_col=0)
+
+# Pick random training img indicies
+training_img_idx = np.random.randint(0, 73000, n_training_samples)
+training_img_idx = np.sort(training_img_idx)
+print(training_img_idx)
+
+# load relevant annotations
+training_img_annt = utils.load_annotations_dict(training_img_idx)
+
+
+images = nsd_loader.read_images(training_img_idx)
+
+print(images.shape)
+
+print(training_img_annt)
+
+# TODO: Extract Image features using pre-trained VGG16 and save to disk
--- utils.py
+++ utils.py
@@ -1,28 +1,62 @@
-# utility functions
+#Script to create a .json file containing the annotations for all 73k images
+# This way it doesn't need to be recreated every traning loop.
+
+# Will store the annotations in a dictionary where they key is the image id (idx in the hdf5 file) and the value is a list of all captions for that image.
+
+from nsd_access import NSDAccess
+import numpy as np
+
+nsd_loader = NSDAccess("/home/seagie/NSD")
 
 
-# from a dictionary containing image information, get annotations
-def list_annotations(img_info : list) -> list:
+img_ids = np.arange(0, 73000)
+
+# read the images
+#images = nsd_loader.read_images(img_ids)
+
+def img_idx_to_id(img_idx):
     """
-    Takes the list of dictionaries returned by nsd_access.read_image_coco_info()
-    and extracts just the annotations.
-
-    Returns:
-        list [annotations]
+    Given a (list of) image indicies ( their position in the .hdf5 file ) return their id's
     """
+    img_ids = []
 
-    annotations = []
-    
-    # if we got info on more than 1 image it will be a list of lists
-    if any(isinstance(el, list) for el in img_info):
-        # loop through list
-        for i in range(0, len(img_info)):
-            img_annotations = []
-            for j in range(0, len(img_info[i])):
-                img_annotations.append(img_info[i][j]['caption'])
-            annotations.append(img_annotations)
+    if isinstance(img_idx, list):
+        for i in img_idx:
+            img_idx.append( )
     else:
-        for i in range(0, len(img_info)):
-            annotations.append(img_info[i]['caption']) 
+        img_ids.append()
 
-    return annotations
+    return img_ids
+
+
+def load_annotations_dict(img_ids: list, key_id = False):
+    """
+    given image id's (indices in hdf5 file) load the relevant captions into a dictionary key'd by the image id.    
+
+    If key_id = True then use the image ID as the key, otherwise use its index from the .hdf5 file.
+    """
+
+    nsd_loader = NSDAccess("/home/seagie/NSD")
+    
+    annotations = nsd_loader.read_image_coco_info(img_ids, info_type='captions', show_annot=False, show_img=False)
+
+    annot_dict = {}
+
+    for i in range(0, len(annotations)):
+        annot_list = []
+        img_id = None
+        for j in range(0, len(annotations[i])):
+            annot_list.append(annotations[i][j]['caption'])
+            # get the current images ID
+            if j == 0:
+                if key_id:
+                    img_id = annotations[i][j]['id']
+                else:
+                    img_id = img_ids[i]
+
+        # store annotations to dictionary    
+        annot_dict[img_id] = annot_list
+
+    return annot_dict
+
+
